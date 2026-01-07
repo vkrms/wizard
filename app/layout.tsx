6c196ae53ee9d58db956a57e930b83280e3b4995
Pavel M <april504@yandex.ru>
2026-01-07 20:42:36 +0700

    feat: update app layout and styles
    - Add custom Tailwind styles and CSS variables
    - Update root layout with custom font
    - Simplify home page to redirect to wizard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Ubuntu } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const ubuntu = Ubuntu({
+  variable: "--font-ubuntu",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  // regular, medium, bold
+  weight: ["400", "500", "700"],
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${ubuntu.variable} antialiased p-6`}
       >
         {children}
       </body>
